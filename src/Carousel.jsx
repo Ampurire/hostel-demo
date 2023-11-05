1decc62cc418d2099b6dfcfa33a8f002cf5096ea
ampurire joan <kiizajoan66@gmail.com>
2023-11-05 18:12:19 +0300

    added some addtioal infomation

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -3,56 +3,94 @@ import Carouselitem from "./Carouselitem";
 import "./index.css";
 
 const Carousel = () => {
+  //State to manage the active index of the carousel
   const [activeIndex, setActiveIndex] = useState(0);
 
+  // define an array of items each representing a hostel
   const items = [
     {
       title: "Nakiyingyi Hostel",
+      location: "kikoni",
       description: "Has both selfcontained and non-selfcontained rooms",
+      singleRoomPrice: "Shs 1,400,000",
+      doubleRoomPrice: "Shs 950,000",
+      roomTypes: "Single and Double Rooms",
       icon: require("./Media/image1.jpg"),
     },
     {
       title: "Olympia Hostel",
-      description: "Has both selfcontained and non-selfcontained rooms",
+      location: "kikoni",
+      description: "Has a swimming pool",
+      singleRoomPrice: "Shs 1,700,000",
+      doubleRoomPrice: "Shs 1,150,000",
+      roomTypes: "Single and Double Rooms",
       icon: require("./Media/image2.jpg"),
     },
     {
       title: "JJ hostel",
+      location: "kikoni, near Nakiyingyi hostel",
       description: "Has both selfcontained and non-selfcontained rooms",
+      singleRoomPrice: "Shs 1,350,000",
+      doubleRoomPrice: "Shs 1,150,000",
+      roomTypes: "Single and Double Rooms",
       icon: require("./Media/image3.jpg"),
     },
     {
       title: "Makerere Garden Courts Hostel",
-      description: "Has both selfcontained and non-selfcontained rooms",
+      location: "kikoni",
+      description: "in a quiet neighborhood",
+      singleRoomPrice: "Shs 1,200,000",
+      doubleRoomPrice: "Shs 850,000",
+      roomTypes: "Single and Double Rooms",
       icon: require("./Media/image4.jpg"),
     },
     {
       title: "Kare Hostel",
-      description: "Has both selfcontained and non-selfcontained rooms",
+      location: "kikoni",
+      description: "It is surrounded by good restaurants",
+      singleRoomPrice: "Shs 1,150,000",
+      doubleRoomPrice: "Shs 950,000",
+      roomTypes: "Single and Double Rooms",
       icon: require("./Media/image5.jpg"),
     },
     {
       title: "Kann Hostel",
+      location: "near Makerere western gate",
       description: "Has both selfcontained and non-selfcontained rooms",
+      singleRoomPrice: "Shs 1,200,000",
+      doubleRoomPrice: "Shs 900,000",
+      roomTypes: "Single and Double Rooms",
       icon: require("./Media/image6.jpg"),
     },
     {
       title: "Prince Hostel",
+      location: "kikoni, after kann hostel",
       description: "Has both selfcontained and non-selfcontained rooms",
+      singleRoomPrice: "Shs 1,000,000",
+      doubleRoomPrice: "Shs 850,000",
+      roomTypes: "Single and Double Rooms",
       icon: require("./Media/image7.jpg"),
     },
     {
       title: "Sunways Hostel",
-      description: "Has both selfcontained and non-selfcontained rooms",
+      location: "Opposite Makerere western gate",
+      description: "Can easily access public transport",
+      singleRoomPrice: "Shs 1,200,000",
+      doubleRoomPrice: "Shs 950,000",
+      roomTypes: "Single and Double Rooms",
       icon: require("./Media/image8.jpg"),
     },
     {
       title: "New-Nana Hostel",
-      description: "Has both selfcontained and non-selfcontained rooms",
+      location: "Near Makerer Main gate",
+      description: "Has the biggest shuttles",
+      singleRoomPrice: "Shs 1,900,000",
+      doubleRoomPrice: "Shs 1,150,000",
+      roomTypes: "Single and Double Rooms",
       icon: require("./Media/image9.jpg"),
     },
   ];
-
+// Function to update the active index of the carousel
   const updateIndex = (newIndex) => {
     if (newIndex < 0) {
       newIndex = 0;
--- src/Carouselitem.jsx
+++ src/Carouselitem.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-  const Carouselitem = ({ item }) => {
-    return (
-      <div className="carousel-item">
-        <img className="carousel-img" src={item.icon} alt={item.title} />
-        <p className="carousel-item-text">{item.description}</p>
+const Carouselitem = ({ item }) => {
+  return (
+    <div className="carousel-item">
+      <img className="carousel-img" src={item.icon} alt={item.title} />
+      <div className="carousel-item-text">
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+        <p>Location: {item.location}</p>
+        <p>Room Types: {item.roomTypes}</p>
+        <p>Single Room Price: {item.singleRoomPrice}</p>
+        <p>Double Room Price: {item.doubleRoomPrice}</p>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
 
-export default Carouselitem
+export default Carouselitem;
